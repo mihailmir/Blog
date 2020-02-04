72dc04c2401cf1fa044c8f61c15b6e9a7082fc05
mihailm <mikhail.miroshnichenko18@gmail.com>
2020-02-04 18:51:10 +0200

    code refactoring

--- static/article.js
+++ static/article.js
@@ -1,16 +1,16 @@
 
 var ArticlePage = (function() {
-    self = this;
-    init = function (article_id, page, urls) {
-        self.article_id = article_id;
-        self.page = page;
-        self.urls = urls;
-        self.update();
+    let initial_data = {}
+    let init = function (article_id, page, urls) {
+        initial_data.article_id = article_id;
+        initial_data.page = page;
+        initial_data.urls = urls;
+        update_comments.call(initial_data);
     }
-    update = function() {
-        $.ajax(self.urls["view_comment"], {
+    function update_comments() {
+        $.ajax(this.urls["view_comment"], {
             type: 'get',
-            data: { 'article_id': self.article_id, 'page': self.page},
+            data: { 'article_id': this.article_id, 'page': this.page},
             dataType: 'html',
             success : function(html) {
                 $(".comment-block").html(html);
@@ -20,13 +20,28 @@ var ArticlePage = (function() {
             }
         });
     }
-    add_comment = function() {
+    function add_comment(form) {
         $.ajax(form.attr('action'), {
-            type: self.form.attr('method'),
-            data: self.form.serialize(),
+            type: form.attr('method'),
+            data: form.serialize(),
             success : function() {
-                self.update();
+
+                update_comments.call(initial_data);
                 form.trigger('reset');
+            },
+            error: function() {
+                alert("Error");
+            }
+        });
+    }
+    function reply_comment(comment_id) {
+        let article_id = this.article_id;
+        $.ajax(this.urls["add_comment"], {
+            type: 'get',
+            data: { 'article_id': article_id, 'comment_id': comment_id},
+            dataType: 'html',
+            success : function(html) {
+                $(".reply-form-" + comment_id).html(html);
             },
             error: function() {
                 alert("Error");
@@ -35,14 +50,13 @@ var ArticlePage = (function() {
     }
     $(document).on('submit', '.add-comment' , function(e) {
         e.preventDefault();
-        self.form = $(this);
-        add_comment();
+        add_comment.call(initial_data, $(this));
     });
 
     $(document).on('click', '.view-next, .view-prev' , function(e) {
         e.preventDefault();
-        self.page = $(this).attr('page');
-        self.update();
+        initial_data.page = $(this).attr('page');
+        update_comments.call(initial_data);
     });
 
     $(document).on('click', '.reply-button' , function(e) {
@@ -51,17 +65,7 @@ var ArticlePage = (function() {
         let comment_container = $(".reply-form-" + comment_id);
 
         if ($(comment_container).is(':empty')){
-            $.ajax(self.urls["add_comment"], {
-                type: 'get',
-                data: { 'article_id': self.article_id, 'comment_id': comment_id},
-                dataType: 'html',
-                success : function(html) {
-                    $(".reply-form-" + comment_id).html(html);
-                },
-                error: function() {
-                    alert("Error");
-                }
-            });
+            reply_comment.call(initial_data, comment_id)
         }
         else if ($(comment_container).is(':hidden')){
             comment_container.show();
@@ -70,6 +74,6 @@ var ArticlePage = (function() {
             comment_container.hide();
         }
     });
-    return {'init': init, 'update': update};
+    return {'init': init}
 }());
 
